d96655cd422d50b3498e732d50ed8c5570580cb8
drunkbug <leyiqiang@gmail.com>
2017-03-16 16:05:34 -0400

    implement tcp pack

--- project4/tcp.py
+++ project4/tcp.py
@@ -24,4 +24,71 @@ class TCPSocket(object):
     # packet to construct
     packet = ''
 
+    def __init__(self, src_ = '', src_port_ = '', dst_ = ''):
+        # set up src and dst ip and port
+        self.src_port = src_port_
+        self.src_id = src_
+        self.dst_port = 80
+        self.dst_id = dst_
+        
+        tcp_seq = 0
+        tcp_ack_seq = 0
+        tcp_doff = 5
+        #tcp flags
+        tcp_fin = 0
+        tcp_syn = 0
+        tcp_rst = 0
+        tcp_psh = 0
+        tcp_ack = 0
+        tcp_urg = 0
+        tcp_window = socket.htons (5840) # for flow control
+        tcp_checksum = 0
+        tcp_urg_ptr = 0
 
+        tcp_offset_res = (tcp_doff << 4) + 0
+
+        self.format = '!HHLLBBHHH'
+        self.psh_format = '!4s4sBBH'
+
+    def pack(self, usrdata = ''):
+        tcp_flags = self.tcp_fin + (self.tcp_syn << 1) + (self.tcp_rst << 2) + (self.tcp_psh <<3) + (self.tcp_ack << 4) + (self.tcp_urg << 5)
+        tcp_header = pack(self.format, \
+                        self.src_port, \
+                        self.dst_port, \
+                        self.tcp_seq, \
+                        self.tcp_ack_seq, \
+                        self.tcp_offset_res, \
+                        tcp.flags, \
+                        self.tcp_window, \
+                        self.tcp_checksum, \
+                        self.tcp_urg_ptr)
+        # pseudo header fields
+        source_addr = socket.inet_aton( self.src_id )
+        dest_addr = socket.inet_aton( self.dst_id )
+        placeholder = 0
+        protocol = socket.IPPROTO_TCP
+        tcp_length = len(tcp_header) + len(usrdata)
+
+        psh = pack(self.psh_format, \
+                    source_addr, \
+                    dest_addr, \
+                    placeholder, \
+                    protocol, \
+                    tcp_length)
+        psh = psh + tcp_header + user_data
+
+        tcp_check = checksum(psh)
+
+        # tcp header with checksum
+        tcp_header = pack('!HHLLBBH', \
+                        self.src_port, \
+                        self.dst_port, \
+                        self.tcp_seq, \
+                        self.tcp_ack_seq, \
+                        self.tcp_offset_res, \
+                        tcp.flags, \
+                        self.tcp_window) + \
+                     pack('H', self.tcp_checksum) + \
+                     pack('!H', self.tcp_urg_ptr)
+
+       return tcp_header +  usrdata
