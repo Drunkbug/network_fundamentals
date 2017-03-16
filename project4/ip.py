2e6cbbf6d770db7e4808e4e900cfc69ab8edc79f
drunkbug <leyiqiang@gmail.com>
2017-03-16 14:24:41 -0400

    fix typo

--- project4/ip.py
+++ project4/ip.py
@@ -1,7 +1,41 @@
 #!/bin/python3
 import socket, sys
+from random import randint
 from util import get_source_ip, parse_raw_url, checksum, get_valid_port
-class IPv4PacketManager(object):
+class IPSocket(object):
+    def __init__(self, dest_ = ''):
+        # create a raw send and receive socket
+        # a send socket must be of type SOCK_RAW/IPPROTO_RAW
+        try:
+            self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        except socket.error as se:
+            raise se
+            sys.exit()
+
+        # a receive socket must be of type SOCK_STREAM/IPPROTO_IP
+        try:
+            self.receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+        except socket.error as se:
+            raise se
+            sys.exit()
+
+        self.dest_ip = dest_
+
+        self.src_ip = get_source_ip()
+        self.src_port = get_valid_port()
+
+    # send and receive data
+    # based on ISOOSI model, network layer is under transport layer, so we implement socket connection here
+    def send(self, dest_ip_, data_):
+        self.dest_ip = dest_ip_
+        raw_packet = IPv4Packet()
+        packet = raw_packet.pack(data_)
+        self.send_socket.sendto(packet, (self.dest_ip, self.src_port))
+    # receive datagram
+    def receive(self):
+        print ("")
+
+class IPv4Packet(object):
     def __init__(self, src = '', dest = '', dat = ''):
         # ip header fields
         self.ip_ihl = 5
@@ -13,15 +47,16 @@ class IPv4PacketManager(object):
         self.ip_frag_off = 0 
 
         self.ip_ttl = 255 # time to live
-        self.ip_proto = socket.IPROTO_TCP
+        self.ip_proto = socket.IPPROTO_TCP
         self.ip_checksum = 0 # Let checksum be 0 first then calculate later 
         self.src_ip = src#socket.inet_aton (self.src_ip)
         self.dest_ip = dest#socket.inet_aton (dest_ip)
         self.data = dat
         self.format = '!BBHHHBBH4s4s'
 
-        self.ip_ihl_ver = (ip_ver << 4) + ip_ihl
+        self.ip_ihl_ver = (self.ip_ver << 4) + self.ip_ihl
         self.data = dat
+
 
     # packet the header
     def pack(self, data):
@@ -52,18 +87,12 @@ class IPv4PacketManager(object):
                         self.ip_checksum, \
                         ip_saddr, \
                         ip_daddr)
-        return ip_header + self.data
+        return ip_header + data
 
     # unpack and validate received data
     def unpack(self, data):
         ip_fields = struct.unpack(self.format, data[:20]) 
         ip_ihl = ip_fields[0] - (self.ip_ver << 4)
 
-    # send datagram
-    def send(self):
-        print ""
 
-    # receive datagram
-    def receive(self):
-        print ""
 
--- project4/tcp.py
+++ project4/tcp.py
@@ -3,9 +3,6 @@
 import socket, sys
 from util import get_source_ip, parse_raw_url, checksum, get_valid_port
 class TCPSocket(object):
-    # send and receive socket
-    send_socket = None
-    receive_socket = None
     seq_num = 0
     ack_num = 0
     # buffer to handle out-of-order packets and identify dup pkts
@@ -26,27 +23,5 @@ class TCPSocket(object):
     #dst_ip = ''
     # packet to construct
     packet = ''
-    # format
-    ip_header_format = '!BBHHHBBH4s4s'
 
-    # init function for TCPSub class
-    def __init__(self):
-        # create a raw send and receive socket
-        # a send socket must be of type SOCK_RAW/IPPROTO_RAW
-        try:
-            self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-        except (socket.error, msg):
-            print ('Send socket could not be created. Error Code: ' + str(msg[0]) + ' Message ' + msg[1])
-            sys.exit()
-
-        # a receive socket must be of type SOCK_STREAM/IPPROTO_IP
-        try:
-            self.receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
-        except (socket.error, msg):
-            print ('Receive socket could not be created. Error Code: ' + str(msg[0]) + ' Message ' + msg[1])
-            sys.exit()
-
-        self.src_ip = get_source_ip()
-        self.src_port = get_valid_port()
-    # end init
 
