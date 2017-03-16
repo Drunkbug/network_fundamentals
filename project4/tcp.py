e2bee6a97b236e430bd7042609ffdc7e53e0fa9e
drunkbug <leyiqiang@gmail.com>
2017-03-16 16:06:52 -0400

    delete duplicate fields from TCPPack class

--- project4/tcp.py
+++ project4/tcp.py
@@ -3,26 +3,11 @@
 import socket, sys
 from util import get_source_ip, parse_raw_url, checksum, get_valid_port
 class TCPSocket(object):
-    seq_num = 0
-    ack_num = 0
-    # buffer to handle out-of-order packets and identify dup pkts
-    send_buffer = ''
-    recv_buffer = ''
-    # TCP flow control
-    # TODO set a fit number
-    adv_wnd = 0
     # TCP congestion control
     cwnd = 1
     max_cwnd = 1000
      # set timeout in 60 seconds, if rto set cwnd to 1
     rto = 60
-    # source and destination ip
-    src_ip = ''
-    # need to select a random free port from 1024 to 65535
-    src_port = -1
-    #dst_ip = ''
-    # packet to construct
-    packet = ''
 
     def __init__(self, src_ = '', src_port_ = '', dst_ = ''):
         # set up src and dst ip and port
