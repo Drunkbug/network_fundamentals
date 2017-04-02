1e207e3deac4ff5e058eb2c0edd098c37880a6b2
drunkbug <leyiqiang@gmail.com>
2017-04-02 16:37:49 -0400

    http cache

--- project5/httpserver.py
+++ project5/httpserver.py
@@ -1,4 +1,41 @@
 import sys
+import os
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+class HttpServerHandler(BaseHTTPrequestHandler):
+    def __init__(self, port_, origin_):
+        self.port = port_
+        self.origin = origin_
+        
+    def httpGET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        # self.path: request path
+        # wfile writing a response back to the client
+        # use self.path to check if local cache
+        # if yes wfile cache
+        # else get from origin server
+
+        # os.path.isdir
+        cache_path = os.pardir + self.path
+        if(os.path.exists(cache_path)):
+            self.response(cache_path)
+        else:
+            cache_path = cacheFile(self.path)  
+            self.response(cache_path)
+
+    def response(self, path):
+        f = open(cache_path, 'r')
+        self.wfile.write(f.read())
+        f.close()
+        
+
+            
+    def cacheFile(self, path):
+        print("todo")  
+        return
+
 
 def argparse(inputs):
     port = 0
@@ -22,5 +59,12 @@ def argparse(inputs):
         sys.exit("Invalid port number. Range: 40000-65535.")
     return port, origin 
         
+def run(port, origin, server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_handler = HttpServerHandler(port, origin)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    https.serve_forever()
 
 port, origin = argparse(sys.argv)
+run(port, origin)
+
