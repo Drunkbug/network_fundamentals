02748eb5912ff85b79dbf762ecef51d596dd1d1c
drunkbug <leyiqiang@gmail.com>
2017-04-03 19:55:12 -0400

    dns message

--- project5/dnsmessage.py
+++ project5/dnsmessage.py
@@ -4,7 +4,28 @@ import struct
 class DNSMessageHandler(object):
 
     def __init__(self):
-        self.id = 0
+        self.dns_header_data = ''
+        self.dns_question_data = ''
+        self.dns_answer_data = ''
+
+    def fetch(self, data):
+        self.dns_header_data = data[0:12]
+        self.dns_question_data = data[12:]
+
+    def build_header_data(self, header_data):
+        dns_header = DNSHeader()
+        dns_header.build(header_data)
+        return dns_header 
+
+    def build_question_data(self, question_data):
+        dns_question = DNSQuestion()
+        dns_question.build()
+        return dns_question 
+
+    def build_answer_data(self, answer_data):
+        dns_answer = DNSAnswer()
+        dns_answer.pack()
+        return dns_answer 
 
 class DNSHeader(object):
 
@@ -17,10 +38,17 @@ class DNSHeader(object):
         self.arcount = 0
 
     def fetch(self, data):
-        return
+        self.id,
+        self.flags,
+        self.qdcount,
+        # number of items in answer section
+        self.ancount,
+        self.nscount,
+        self.arcount = struct.unpack('!hhhhhh', data)
 
     def pack(self):
-       header_packet = struct.pack('!hhhhhh', 
+        self.ancount = 1 
+        header_packet = struct.pack('!hhhhhh', 
                                     self.id, 
                                     self.flags, 
                                     self.qdcount, 
@@ -28,6 +56,10 @@ class DNSHeader(object):
                                     self.nscount, 
                                     self.arcount)
        return header_packet
+
+    def build(self, data):
+        self.fetch(data)
+        self.pack()
 
 class DNSQuestion(object):
 
@@ -37,13 +69,22 @@ class DNSQuestion(object):
         self.qclass = 0
 
     def fetch(self, data):
-        return
+        print (len(data))
+        print (data)
+        self.qtype,
+        self.qclass,
+        ending = struct.unpack('!hh', data)
 
     def pack(self):
         question_packet = struct.pack('!hhh', 
                                        self.qtype, 
                                        self.qclass)
         return question_packet
+
+    def build(self, data):
+        self.fetch(data)
+        self.pack()
+
 
 class DNSAnswer(object):
 
