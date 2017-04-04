b53522a91c5f4e02cdb806e8e1f126821856a1b8
drunkbug <leyiqiang@gmail.com>
2017-04-04 02:05:20 -0400

    get rid of SocketServer library

--- project5/dnsserver.py
+++ project5/dnsserver.py
@@ -1,36 +1,27 @@
 import sys
 from util import *
 from dnsmessage import DNSMessageHandler
-from SocketServer import UDPServer, BaseRequestHandler
-#from socketserver import UDPServer, BaseRequestHandler
+import socket
 
 # read and parse inputs
 inputs = sys.argv
 PORT, DOMAIN= parse_dns_server_input(inputs)
 RECORD = 'ec2-54-85-79-138.compute-1.amazonaws.com'
 
-class MyUDPHandler(BaseRequestHandler):
-    # use dig/drill @<ip> cs5700cdn.example.com -p <port> to test
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        # TODO choose best replica
-        client_ip = format(self.client_address[0])
+udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_server.bind(('', PORT))
+
+try:
+    while 1:
+        data, client_address = udp_server.recvfrom(1024)
+        print ("data:")
+        print (data)
+        print (client_address)
         ip_address = '54.85.79.138'
-        
         dns_message_handler = DNSMessageHandler(DOMAIN, ip_address)
         dns_message_packet = dns_message_handler.build_dns_message(data)
         print repr(dns_message_packet)
-
-        socket.sendto(dns_message_packet, self.client_address)
-
-
-# establish DNS server
-server = UDPServer(('', PORT), MyUDPHandler)
-# start DNS server
-try:
-    server.serve_forever()
+        udp_server.sendto(dns_message_packet, client_address)
 except KeyboardInterrupt:
-    server.shutdown()
-    sys.exit(0)
+    udp_server.close()
 
