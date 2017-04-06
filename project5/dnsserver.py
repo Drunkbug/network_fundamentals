8617af7cdb97bbc2fd1536874e369b382cf228f6
drunkbug <leyiqiang@gmail.com>
2017-04-06 16:33:23 -0400

    dns server comments

--- project5/dnsserver.py
+++ project5/dnsserver.py
@@ -3,25 +3,32 @@ from util import *
 from dnsmessage import DNSMessageHandler
 import socket
 
-# read and parse inputs
-inputs = sys.argv
-PORT, DOMAIN= parse_dns_server_input(inputs)
-RECORD = 'ec2-54-85-79-138.compute-1.amazonaws.com'
 
 class DNSServer(object):
 
+    """ DNS server object
+    establish and run the server with socket
+
+    Attributes:
+        udp_server: a socket object for establishing dns server
+    """
     def __init__(self):
         self.udp_server = None
 
     def build_server(self):
+        """ init dns server class with socket"""
         self.udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_server.bind(('', PORT))
 
     def serve_forever(self):
+        """ parsing client information and request
+            send dns message back to client
+        """
         try:
             while 1:
                 data, client_address = self.udp_server.recvfrom(1024)
                 ip_address = '54.85.79.138'
+                # parse and bulid dns message
                 dns_message_handler = DNSMessageHandler(DOMAIN, ip_address)
                 dns_message_packet = dns_message_handler.build_dns_message(data)
                 print repr(dns_message_packet)
@@ -30,7 +37,14 @@ class DNSServer(object):
             self.udp_server.close()
             sys.close(0)
 
-dns_server = DNSServer()                
-dns_server.build_server()
-dns_server.serve_forever()
 
+if __name__ == '__main__':
+    # read and parse inputs
+    inputs = sys.argv
+    PORT, DOMAIN= parse_dns_server_input(inputs)
+    RECORD = 'ec2-54-85-79-138.compute-1.amazonaws.com'
+    # start dns server
+    dns_server = DNSServer()                
+    dns_server.build_server()
+    dns_server.serve_forever()
+    
